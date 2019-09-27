0cb1acb1201064728a91d8218d85b8c1853e5f81
neolao <contact@neolao.com>
2019-09-27 21:52:50 +0000

    update zsh install

--- setups/zsh/oh-my-zsh-install.sh
+++ setups/zsh/oh-my-zsh-install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo ""
-echo "- Install ZSH: Oh My Zsh ... "
+if [ -f ~/.oh-my-zsh-mounted/oh-my-zsh.sh ]
+then
+    ln -s ~/.oh-my-zsh-mounted ~/.oh-my-zsh
+else
+    echo ""
+    echo "- Install ZSH: Oh My Zsh ... "
 
-. /setups/util/trace_output.sh
-(
-    if [ -f ~/.oh-my-zsh-mounted/oh-my-zsh.sh ]
-    then
-        ln -s ~/.oh-my-zsh-mounted ~/.oh-my-zsh
-    else
+    . /setups/util/trace_output.sh
+    (
         sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
         cp -r ~/.oh-my-zsh/* ~/.oh-my-zsh-mounted
         cp -r ~/.oh-my-zsh/.* ~/.oh-my-zsh-mounted
-    fi
-) 2>&1 | trace_output
+    ) 2>&1 | trace_output
+fi
 
--- setups/zsh/theme-install.sh
+++ setups/zsh/theme-install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo ""
-echo "- Install ZSH: Theme ... "
+if [ ! -d ~/.oh-my-zsh-custom/themes/powerlevel9k ]
+then
+    echo ""
+    echo "- Install ZSH: Theme ... "
 
-. /setups/util/trace_output.sh
-(
-    if [ ! -d ~/.oh-my-zsh-custom/themes/powerlevel9k ]
-    then
+    . /setups/util/trace_output.sh
+    (
         git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh-custom/themes/powerlevel9k
-    fi
-) 2>&1 | trace_output
+    ) 2>&1 | trace_output
+fi
 
